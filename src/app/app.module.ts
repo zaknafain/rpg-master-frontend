80d4782caaf07b5ffd1db9cac41ac412832c119c
David Seeherr <david.seeherr@gmail.com>
2018-04-03 21:25:26 +0200

    Added sign-in

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 
 import { AppComponent } from './app.component';
+import { SignInComponent } from './sign-in/sign-in.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInComponent
   ],
   imports: [
     BrowserModule,
