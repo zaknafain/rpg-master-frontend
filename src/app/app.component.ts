97e5651f6bb4fd9ecb5974c0db6ce3e8a9537147
David Seeherr <david.seeherr@gmail.com>
2018-04-13 09:20:46 +0200

    Removed remnants of log dialog

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,11 @@
 import { Component, ChangeDetectorRef } from '@angular/core';
 import { MediaMatcher } from '@angular/cdk/layout';
-import { MatDialog } from '@angular/material/dialog';
 
 import { User } from './models/user';
 
 import { AuthService } from './services/auth.service';
 import { MessageService } from './services/message.service';
 import { UserService } from './services/user.service';
-
-import { LogDialog } from './log-dialog/log-dialog.component';
 
 @Component({
   selector: 'app-root',
@@ -27,8 +24,7 @@ export class AppComponent {
     private userService: UserService,
     private messageService: MessageService,
     private changeDetectorRef: ChangeDetectorRef,
-    private media: MediaMatcher,
-    public logDialog: MatDialog
+    private media: MediaMatcher
   ) {
     this.mobileQuery = media.matchMedia('(max-width: 600px)');
     this._mobileQueryListener = () => changeDetectorRef.detectChanges();
