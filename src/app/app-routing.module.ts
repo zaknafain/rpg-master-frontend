a80db63a085c9efd51b403a8adace8a9527891f3
David Seeherr <david.seeherr@gmail.com>
2022-03-31 17:09:45 +0200

    Move the campaign list into a route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CampaignListComponent } from './campaigns/campaign-list/campaign-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'campaigns', component: CampaignListComponent },
+  { path: '',   redirectTo: '/campaigns', pathMatch: 'full' }, // redirect to `first-component`
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
