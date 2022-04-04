911651924e0be5f0b9911fb0843101484955d8e0
David Seeherr <david.seeherr@gmail.com>
2022-04-04 17:52:11 +0200

    Create MaterialModule to bundle Material resources

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,33 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MatButtonModule } from '@angular/material/button';
-import { MatExpansionModule } from '@angular/material/expansion';
-import { MatIconModule } from '@angular/material/icon';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatToolbarModule } from '@angular/material/toolbar';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { CampaignListComponent } from './campaigns/campaign-list/campaign-list.component';
+import { MaterialModule } from './material/material.module';
 
 @NgModule({
   declarations: [
     AppComponent,
+    CampaignListComponent,
     FooterComponent,
-    CampaignListComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule,
-    MatExpansionModule,
-    MatIconModule,
-    MatSidenavModule,
-    MatToolbarModule,
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
