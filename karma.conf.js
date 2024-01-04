9264d7b1e921c940053c3205f08b44a719632278
David Seeherr <david.seeherr@gmail.com>
2024-01-04 11:57:22 +0100

    Update Angular to 15

--- karma.conf.js
+++ karma.conf.js
@@ -37,7 +37,13 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome', 'ChromeHeadless'],
+    browsers: ['Chrome', 'ChromeHeadless', 'ChromeHeadlessNoHeadless'],
+    customLaunchers: {
+      ChromeHeadlessNoHeadless: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
     singleRun: false,
     restartOnFileChange: true
   });
